87940bd69897f03bf54d343cc03611bdda8b03ac
mehraneh1234 <132540431+mehraneh1234@users.noreply.github.com>
2023-11-09 01:57:10 +0800

    update priority, display, express
    update priority, display, express

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,25 +21,37 @@ namespace ServiceProcess
     /// </summary>
     public partial class MainWindow : Window
     {
-        Drone[] droneInstanceA = new Drone[5];
+        
         
         Queue<Drone> regularServiceQueue = new Queue<Drone>();
         Queue<Drone> expressServiceQueue = new Queue<Drone>();
         List<Drone> finishedList = new List<Drone>();
+        private int currentTag = 100;
 
 
         public MainWindow()
         {
             InitializeComponent();
+            FillComboBox(); 
+            
         }
 
-       
+       private void FillComboBox()
+        {
+            for (int i = 100; i <= 900; i +=10)
+            {
+                cbTag.Items.Add(i);
+            }
+            // Set the default value to 100
+            cbTag.SelectedIndex = 0; 
+        }
 
         
 
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
-            if(!string.IsNullOrEmpty(tbClientName.Text) &&
+            tbServiceCost.Text = "125";
+            if (!string.IsNullOrEmpty(tbClientName.Text) &&
                 !string.IsNullOrEmpty(tbDroneModel.Text) &&
                 !string.IsNullOrEmpty(tbServiceProblem.Text) &&
                 !string.IsNullOrEmpty(tbServiceCost.Text))
@@ -47,15 +60,17 @@ namespace ServiceProcess
                 droneInstance.SetClientName(tbClientName.Text);
                 droneInstance.SetDroneModel(tbDroneModel.Text);
                 droneInstance.SetServiceProblem(tbServiceProblem.Text);
-                droneInstance.SetServiceCost(double.Parse(tbServiceCost.Text));
+                droneInstance.SetServiceCost(Double.Parse(tbServiceCost.Text));
                 droneInstance.SetServiceTag(cbTag.Text);
 
-                if (rbExpress.IsChecked == true)
+                if (GetPriority() == 1)
                 {
-                    droneInstance.SetServiceCost(Convert.ToDouble(tbServiceCost) * 1.15);
+                    droneInstance.SetServiceCost(Double.Parse(tbServiceCost.Text) * 1.15);
                     expressServiceQueue.Enqueue(droneInstance);
                     DisplayExpressService();
                     ClearTextboxes();
+                  
+                    
                 }
                 else
                 {
@@ -63,7 +78,7 @@ namespace ServiceProcess
                     DisplayRegularService();
                     ClearTextboxes();
                 }
-                
+                IncrementTag();
             }
             else
             {
@@ -71,18 +86,19 @@ namespace ServiceProcess
             }
         }
 
-        /* private void GetPriority()
-         {
-             int priority = 0;
-             if (rbRegular.IsChecked == true)
-             {
-                 priority = 0;
-             }
-             else if (rbExpress.IsChecked == true)
-             {
-                 priority = 1;
-             }
-         }*/
+        private int GetPriority()
+        {
+            int priority = 0;
+            if (rbRegular.IsChecked == true)
+            {
+                priority = 0;
+            }
+            else if (rbExpress.IsChecked == true)
+            {
+                priority = 1;
+            }
+            return priority;
+        }
 
         public void DisplayRegularService()
         {
@@ -102,10 +118,10 @@ namespace ServiceProcess
 
         public void DisplayExpressService()
         {
-            lvRegularService.Items.Clear();
-            foreach (Drone dr in regularServiceQueue)
+            lvExpressService.Items.Clear();
+            foreach (Drone dr in expressServiceQueue)
             {
-                lvRegularService.Items.Add(new
+                lvExpressService.Items.Add(new
                 {
                     Name = dr.GetClientName(),
                     Model = dr.GetDroneModel(),
@@ -153,17 +169,28 @@ namespace ServiceProcess
         {
             tbClientName.Clear(); tbClientName.Foreground = Brushes.CadetBlue; tbClientName.Text = "Enter Full Name";
             tbDroneModel.Clear(); tbDroneModel.Foreground = Brushes.CadetBlue; tbDroneModel.Text = "Enter Drone Model";
-            tbServiceCost.Clear(); tbServiceCost.Foreground = Brushes.CadetBlue; tbServiceCost.Text = "Enter Service Cost";
+            tbServiceCost.Clear(); tbServiceCost.Foreground = Brushes.CadetBlue; tbServiceCost.Text = "125";
             tbServiceProblem.Clear(); tbServiceProblem.Foreground = Brushes.CadetBlue; tbServiceProblem.Text = "Enter Service Problem";
-
+            rbRegular.IsChecked = true;
         }
 
-        /*private void IncrementTag()
+        private void IncrementTag()
         {
-            // int currentTag = (int)Tag.value;
-            //currentTag += 10;
+            //int currentTag = (int)Tag.value;
+            currentTag += 10;
+            if (currentTag <=900)
+            {
+                cbTag.Items.Add(currentTag);
+                cbTag.SelectedItem = currentTag;
+            }
+        }
 
-        }*/
+       
+        private void tbServiceCost_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[.][0-9]+$|^[0-1.]*[.,]{0,1}[0-9]*$");
+            e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
+        }
 
         private void tbClientName_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -171,16 +198,18 @@ namespace ServiceProcess
         }
         private void tbDroneModel_GotFocus(object sender, RoutedEventArgs e)
         {
-            tbDroneModel.Clear(); tbClientName.Foreground = Brushes.Black;
+            tbDroneModel.Clear(); tbDroneModel.Foreground = Brushes.Black;
         }
         private void tbServiceProblem_GotFocus(object sender, RoutedEventArgs e)
         {
-            tbServiceProblem.Clear(); tbClientName.Foreground = Brushes.Black;
+            tbServiceProblem.Clear(); tbServiceProblem.Foreground = Brushes.Black;
         }
         private void tbServiceCost_GotFocus(object sender, RoutedEventArgs e)
         {
-            tbServiceCost.Clear(); tbClientName.Foreground = Brushes.Black;
+            tbServiceCost.Clear(); tbServiceCost.Foreground = Brushes.Black;
         }
+
+        
     }
 
     public class MainDiagram
