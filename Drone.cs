17f4c5e8fcc3d69c2509b6349068d8e5de4d3661
mehraneh1234 <132540431+mehraneh1234@users.noreply.github.com>
2023-11-15 12:29:54 +0800

    finish
    finish

--- Drone.cs
+++ Drone.cs
@@ -7,19 +7,22 @@ using System.Threading.Tasks;
 
 
 namespace ServiceProcess
-{
+{      // A separate class file to hold the data items of the Drone. Use separate getter and setter methods,
+       // ensure the attributes are private and the accessor methods are public. Add a display method that returns a string
+       // for Client Name and Service Cost. Add suitable code to the Client Name and Service Problem accessor methods so the
+       // data is formatted as Title case or Sentence case. Save the class as “Drone.cs”.
     internal class Drone
     {
+        // The default constructor 
         public Drone()
         {
-
-        }
+        }// Decalare private variables
         private string _clientName;
         private string _droneModel;
         private string _serviceProblem;
         private double _serviceCost;
         private string _serviceTag;
-
+        // Getter methods to get the values of the fields from the class
         public string GetClientName()
         {
             return _clientName;
@@ -40,6 +43,7 @@ namespace ServiceProcess
         {
             return _serviceTag;
         }
+        // Setter methods to set the values of the fields in the class
         public void SetClientName(string clientName)
         {
             if (clientName ==null)
@@ -63,7 +67,7 @@ namespace ServiceProcess
         {
             if (serviceCost < 0)
             {
-                _serviceCost = 49.9;
+                _serviceCost = 49.99;
             }
             else
             {
@@ -75,6 +79,7 @@ namespace ServiceProcess
         {
             _serviceTag = serviceTag;
         }
+        // This method is for how display the finished service in the finish list box.
         public string DisplayFinishService()
         {
             return GetClientName() + " : $" + GetServiceCost();
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,26 +17,24 @@ using System.Windows.Shapes;
 namespace ServiceProcess
 {
     /// <summary>
+    /// Mehraneh - 30062786 - AT3 - Drone Srivice Management System
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        
+        // Declare queues and list of a class object 
         Queue<Drone> RegularService = new Queue<Drone>();
         Queue<Drone> ExpressService = new Queue<Drone>();
         List<Drone> FinishedList = new List<Drone>();
-        
-
-
+        // Constructor of the class which calls two methods 
         public MainWindow()
         {
             InitializeComponent();
             FillComboBox(); 
             
         }
-
-       private void FillComboBox()
+        // This method makes a numeric control which minimum value is 100, maximum is 900 with increments of 10. 
+        private void FillComboBox()
         {
             for (int i = 100; i <= 900; i +=10)
             {
@@ -45,24 +43,22 @@ namespace ServiceProcess
             // Set the default value to 100
             cbTag.SelectedIndex = 0; 
         }
-
-        
-
+        // AddNewItem_Click method that will add a new service item to a Queue<> based on the priority
         private void AddNewItem_Click(object sender, RoutedEventArgs e)
         {
-            //tbServiceCost.Text = "125";
+            // To verify that all input controllers are not null
             if (!string.IsNullOrEmpty(tbClientName.Text) &&
                 !string.IsNullOrEmpty(tbDroneModel.Text) &&
                 !string.IsNullOrEmpty(tbServiceProblem.Text) &&
                 !string.IsNullOrEmpty(tbServiceCost.Text))
-            {
+            {// Make an instance of the class to add the new values of the fields
                 Drone droneInstance = new Drone();
                 droneInstance.SetClientName(tbClientName.Text);
                 droneInstance.SetDroneModel(tbDroneModel.Text);
                 droneInstance.SetServiceProblem(tbServiceProblem.Text);
                 droneInstance.SetServiceCost(Double.Parse(tbServiceCost.Text));
                 droneInstance.SetServiceTag(cbTag.Text);
-
+                // If the client chose express then the cost of the srvice will multiply to 1.15 then add the data to the express queue
                 if (GetServicePriority() == 1)
                 {
                     droneInstance.SetServiceCost(Double.Parse(tbServiceCost.Text) * 1.15);
@@ -70,12 +66,12 @@ namespace ServiceProcess
                     DisplayExpressService();
                     ClearTextboxes(); 
                 }
-                else
+                else // If the priority is regular then add the data to the regular queue
                 {
                     RegularService.Enqueue(droneInstance);
                     DisplayRegularService();
                     ClearTextboxes();
-                }
+                }// increment 1 to the tag
                 IncrementTag();
             }
             else
@@ -83,7 +79,7 @@ namespace ServiceProcess
                 MessageBox.Show("PLease enter all data");
             }
         }
-
+        // Declare the priority in regards to which radio buttons is checked.
         private int GetServicePriority()
         {
             int priority = 0;
@@ -97,7 +93,7 @@ namespace ServiceProcess
             }
             return priority;
         }
-
+        // This method displays the regular service queue in the related list view.
         public void DisplayRegularService()
         {
             lvRegularService.Items.Clear();
@@ -113,7 +109,7 @@ namespace ServiceProcess
                 });
             }
         }
-
+        // This method displays the express service queue in the related list view.
         public void DisplayExpressService()
         {
             lvExpressService.Items.Clear();
@@ -129,7 +125,7 @@ namespace ServiceProcess
                 });
             }
         }
-
+        // This method displays the finish service list in the related list box.
         public void DisplayFinishService()
         {
             lbFinishService.Items.Clear();
@@ -138,7 +134,7 @@ namespace ServiceProcess
                 lbFinishService.Items.Add(d.DisplayFinishService());
             }
         }
-
+        // By click on each item in the regular list view, all fields of that row display in the related text boxes.
         private void ListViewDisplayRegular_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (lvRegularService.SelectedIndex != -1)
@@ -149,12 +145,14 @@ namespace ServiceProcess
                 tbServiceProblem.Text = RegularService.ElementAt(index).GetServiceProblem();
                 tbServiceCost.Text = RegularService.ElementAt(index).GetServiceCost().ToString();
                 cbTag.Text = RegularService.ElementAt(index).GetServiceTag();
+                rbRegular.IsChecked = true;
             }
             else
             {
                 lvRegularService.UnselectAll();
             }
         }
+        //  // By click on each item in the express list view, all fields of that row display in the related text boxes.
         private void ListViewDisplayExpress_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
         {
             if (lvExpressService.SelectedIndex != -1)
@@ -165,13 +163,14 @@ namespace ServiceProcess
                 tbServiceProblem.Text = ExpressService.ElementAt(index).GetServiceProblem();
                 tbServiceCost.Text = ExpressService.ElementAt(index).GetServiceCost().ToString();
                 cbTag.Text = ExpressService.ElementAt(index).GetServiceTag();
+                rbExpress.IsChecked = true;
             }
             else
             {
                 lvExpressService.UnselectAll();
             }
         }
-
+        // To clear all text boxes and dispaly after that the related textes
         public void ClearTextboxes()
         {
             tbClientName.Clear(); tbClientName.Foreground = Brushes.CadetBlue; tbClientName.Text = "Enter Full Name";
@@ -180,7 +179,7 @@ namespace ServiceProcess
             tbServiceProblem.Clear(); tbServiceProblem.Foreground = Brushes.CadetBlue; tbServiceProblem.Text = "Enter Service Problem";
             rbRegular.IsChecked = true;
         }
-
+        // By default add 10 to the selected item in the tag combobox.
         private void IncrementTag()
         {
             int currentTag = (int)cbTag.SelectedValue;
@@ -191,14 +190,14 @@ namespace ServiceProcess
                 cbTag.SelectedItem = currentTag;
             }
         }
-
-       
+        // This method makes the Service Cost textbox can only accept a double value with two decimal point.
         private void tbServiceCost_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("^[.][0-9]+$|^[0-1.]*[.,]{0,1}[0-9]*$");
             e.Handled = !regex.IsMatch((sender as TextBox).Text.Insert((sender as TextBox).SelectionStart, e.Text));
         }
-
+        // By clicking on the finish in front of regular list view, the first inserted item of that queue will be deleted 
+        // from the queue and displays in the finish list box.
         private void btnFinishRegular_Click(object sender, RoutedEventArgs e)
         {
             if (RegularService.Count > 0)
@@ -208,7 +207,8 @@ namespace ServiceProcess
                 DisplayFinishService();
             }
         }
-
+        // By clicking on the finish in front of express list view, the first inserted item of that queue will be deleted 
+        // from the queue and displays in the finish list box.
         private void btnFinishExpress_Click(object sender, RoutedEventArgs e)
         {
             if (ExpressService.Count > 0)
@@ -218,7 +218,7 @@ namespace ServiceProcess
                 DisplayFinishService();
             }
         }
-
+        // By double click on each item in the finish list box that item will be deleted from the list.
         private void lbFinishService_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
             if (lbFinishService.SelectedItem != null)
@@ -228,7 +228,7 @@ namespace ServiceProcess
                 DisplayFinishService();
             }
         }
-
+        // By clicking on each text boxes controller the default text will be cleared and the color of the text will be black.
         private void tbClientName_GotFocus(object sender, RoutedEventArgs e)
         {
             tbClientName.Clear(); tbClientName.Foreground = Brushes.Black;
